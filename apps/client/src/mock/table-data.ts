3b8ed3f98cdd0434d56f148cb238199848ed2d74
mohamed <mohamedikramks7@gmail.com>
2024-07-27 16:39:22 +0530

    chore: Update imports and component paths in app.tsx and useIdeas.ts

--- apps/client/src/mock/table-data.ts
+++ apps/client/src/mock/table-data.ts
@@ -1,17 +1,13 @@
-import { Payment } from '@/components/payments-table/columns';
+import { Idea } from '../api/useIdeas';
 
-const payments: Payment[] = [
+
+const payments: Idea[] = [
   {
     id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
+
   },
   {
     id: '489e1d42',
-    amount: 125,
-    status: 'processing',
-    email: 'example@gmail.com',
   },
 ];
 
