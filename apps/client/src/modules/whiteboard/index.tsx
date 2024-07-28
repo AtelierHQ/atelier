885d7a2a07b34d8d0ddc891c19fd14d032ae5b16
Vedansh Wani <vedansh.wani@qapita.com>
2024-07-28 11:29:53 +0530

    implemented a toolbar for the whiteboard

--- apps/client/src/modules/whiteboard/index.tsx
+++ apps/client/src/modules/whiteboard/index.tsx
@@ -1,3 +1,17 @@
+import { useRef, useState } from 'react';
+
+import { Toolbar } from './components';
+import { DrawingConfig, initDrawingConfig } from './types';
+
 export const Whiteboard = () => {
-  return <div>Whiteboard</div>;
+  const [drawingConfig, setDrawingConfig] = useState<DrawingConfig>(initDrawingConfig);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <>
+      <Toolbar activeShape={drawingConfig.toDraw} setToDraw={setDrawingConfig} />
+      <canvas ref={canvasRef}></canvas>
+    </>
+  );
 };
